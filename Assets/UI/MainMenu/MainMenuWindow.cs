6694ce42495e63b073740e57d270f1fbf8efa4fd
Pavel Gordienko <guzmo6@rambler.ru>
2022-06-10 00:49:22 +0300

    add MainMenu

---
+++ Assets/UI/MainMenu/MainMenuWindow.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Net.Mime;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace UI.MainMenu
+{
+    public class MainMenuWindow : AnimatedWindow
+    {
+
+        private Action _closeAction;
+        
+        public void OnShowSettings()
+        {
+            
+        }
+        
+        public void OnStartGame()
+        {
+            //SceneManager.LoadScene("Level1");
+            _closeAction = () => { SceneManager.LoadScene("Level1"); };
+            Close();
+        }
+        
+        public void OnExit()
+        {
+            //Close();
+            _closeAction = () =>
+            {
+                Application.Quit();
+#if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+#endif
+            };
+            Close();
+            
+            
+        }
+        public override void OnCloseAnimationComplete()
+        {
+            base.OnCloseAnimationComplete();
+            _closeAction?.Invoke();
+             
+        }
+    }
+}
